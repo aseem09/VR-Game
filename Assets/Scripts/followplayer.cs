31b1b8f0b6e511df7e1994d3a824f4fbce6f37a5
Aseem Verma <averma2@cs.iitr.ac.in>
2019-01-31 19:53:27 +0530

    GVR Rectile Added

--- Assets/Scripts/followplayer.cs
+++ Assets/Scripts/followplayer.cs
@@ -11,6 +11,6 @@ public class followplayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + offset;
+		transform.position = player.transform.position+offset;
 	}
 }
